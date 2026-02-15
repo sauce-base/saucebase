ab59aca5a2f659d7ed0b4d5db8a2a317de59d80e
roble <renanroble@gmail.com>
2026-02-15 17:59:34 +0000

    refactor: comment out unused Product import and products retrieval in IndexController

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
-use Modules\Billing\Models\Product;
+
+// use Modules\Billing\Models\Product;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
         return Inertia::render('Index', [
-            'products' => Product::displayable()->get(),
+            // 'products' => Product::displayable()->get(),
         ])->withSSR();
     }
 }
