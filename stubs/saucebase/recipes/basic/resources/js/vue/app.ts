9add0582a3f60e0ff989694aa6df81493f2237c7
roble <renanroble@gmail.com>
2026-07-09 20:48:25 +0100

    refactor: remove unused Announcements module setup and related imports

--- stubs/saucebase/recipes/basic/resources/js/vue/app.ts
+++ stubs/saucebase/recipes/basic/resources/js/vue/app.ts
@@ -21,23 +21,3 @@ export function setup() {
 export function afterMount() {
     console.debug('{Module} module after mount logic executed');
 }
-
-////
-
-import { registerGlobalComponent } from '@/lib/globalComponents';
-import '@modules/announcements/resources/css/style.css';
-import AnnouncementBanner from './components/AnnouncementBanner.vue';
-
-/**
- * Announcements module setup
- * Called during app initialization before mounting
- */
-export function setup() {
-    registerGlobalComponent('top', AnnouncementBanner);
-}
-
-/**
- * Announcements module after mount logic
- * Called after the app has been mounted
- */
-export function afterMount() {}
